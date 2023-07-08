1d6872fd6ecf6eff5157f9bd75e8cf8c96339f33
fjjlovelk <2730809743@qq.com>
2023-07-08 14:29:05 +0800

    useMaterial3；去除debug标记；修改默认切换页面动画为cupertino；修改最近任务标题

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import 'app/routes/app_pages.dart';
@@ -7,9 +6,14 @@ import 'app/routes/app_pages.dart';
 void main() {
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "小米商城",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      defaultTransition: Transition.cupertino,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
     ),
   );
 }
